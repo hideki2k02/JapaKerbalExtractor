8b02483281c4d869992a4b105bc31233f6dd7bd7
japa4551 <japa4551@protonmail.com>
2021-06-10 20:37:24 -0300

    1st Public Build

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using Aspose.Zip;
+
+namespace JapaKerbalExtractor { 
+    class Program {
+        static void Main(string[] args) {
+            DirectoryInfo base_dir = new DirectoryInfo(System.AppContext.BaseDirectory);
+            Console.Title = "Japa4551's Kerbal Extractor v1 - Yes the code intentionally has 69 lines";
+
+            if(Directory.Exists("Output")) {
+                DateTime current_time = DateTime.Now;
+                Console.WriteLine("A previously generated GameData folder was found! Renaming it...\n");
+                Directory.Move("Output", $"./Output_{current_time.Hour}-{current_time.Minute}-{current_time.Second}");
+            }
+
+            Console.Write("Please specify the extension of the compressed file(s) (without the .): ");
+
+            string file_extension = Console.ReadLine();
+
+            if(base_dir.GetFiles($"*.{file_extension}").Length < 1 || file_extension.Length == 0) {
+                Console.WriteLine("Silly! There are no files with that extension!");
+                Console.ReadLine(); 
+                Environment.Exit(0);
+            }
+
+            Console.Write($"\nFiles with extension {file_extension} in the current Executable's folder: ");
+            Console.WriteLine(base_dir.GetFiles($"*.{file_extension}").Length);
+
+            Console.WriteLine("Press Enter to extract the files...");
+            Console.ReadLine();
+
+            for(int index = 0; index < base_dir.GetFiles($"*.{file_extension}").Length; index++) {
+                FileInfo[] file_array = base_dir.GetFiles($"*.{file_extension}");
+
+                string current_file_name = Path.GetFileName(Convert.ToString(file_array[index]));
+
+                Console.Write($"Extracting {current_file_name}... ");
+
+                try {
+                    using (var archive = new Archive(current_file_name)) archive.ExtractToDirectory("Output");
+                    
+                    Console.WriteLine("Ok!\n");
+                }
+
+                catch(Exception e) { 
+                    Console.WriteLine(e + "\n");
+                }
+            }
+
+            if(Directory.Exists("Output/GameData")) {
+                Console.WriteLine("GameData folder found! moving files and deleting GameData...");
+
+                foreach(string file in Directory.GetFiles("Output/GameData/")) 
+                { File.Move(file, $"Output/{Path.GetFileName(file)}"); }
+
+                foreach(string directory in Directory.GetDirectories("Output/GameData/")) 
+                { Directory.Move(directory, $"Output/{Path.GetFileName(directory)}"); }
+
+                Directory.Delete("Output/GameData");
+            }
+
+            Console.WriteLine("\nOk! You can close the program now! (Press Enter)"); 
+            Console.WriteLine("Don't forget to check the Output folder for extra files!");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # JapaKerbalExtractor
- A Kerbal Space Program Mod Extraction Tool for lazy people
+
+## Needs [.NET Core 5.0](https://dotnet.microsoft.com/download)
+## Program uses [Aspose.Zip](https://www.nuget.org/packages/Aspose.ZIP/)
+
+A Kerbal Space Program Mod Extraction Tool for lazy people
+
+Since for some reason CurseForge does not install mods properly, i decided to make a (probably) temporary script so i can install multiple mods while being lazy
+
+Fun Fact: I annoyed some people, but the whole code under 69 lines (Commentaries and New Lines are counted on this)
+
+## How to use
+
+- Install [.NET Core 5.0](https://dotnet.microsoft.com/download) if its not installed
+
+- Download the pre-built program on the [Releases page](https://github.com/japa4551/JapaBookmarksDateViewer/releases) (You can also build it yourself if you want to, in case there is no version for your current system)
+
+- The program will attempt to extract **all the files in the same folder as the EXE** with the extension you inputted (tested on RAR and ZIP)
+
+- If it succeeds it will extract the content to a new folder called Output (will be rename in case one already exists)
+
+- In case some of the mods came as "GameData/Mod Folder" instead of just "Mod Folder", the program will fix that for you
+
+- **DONT FORGET TO CHECK FOR ANY EXTRA FILES, you dont need multiple ModuleManager DLLs!**
